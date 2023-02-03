abb8a9095d6cda5243086cc9ce590b7f68cfa074
Helio <helio.btmrq@icloud.com>
2023-02-03 01:27:27 +0000

    Initial Commit

---
+++ 2048/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2048
+//
+//  Created by Hélio Nunes Santos on 2/3/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
